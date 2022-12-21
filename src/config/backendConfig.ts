7f5bf2f7efb6c0083a250af24baeb75df413ceb9
Chunkygoo <calvintai0402@gmail.com>
2022-12-20 22:10:07 -0800

    use own google client IDs

--- src/config/backendConfig.ts
+++ src/config/backendConfig.ts
@@ -84,9 +84,8 @@ export const backendConfig = (): TypeInput => {
         },
         providers: [
           ThirdPartyEmailPasswordNode.Google({
-            clientId:
-              "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-            clientSecret: "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",
+            clientId: env.SUPERTOKENS_GOOGLE_CLIENT_ID,
+            clientSecret: env.SUPERTOKENS_GOOGLE_CLIENT_SECRET,
           }),
         ],
       }),
